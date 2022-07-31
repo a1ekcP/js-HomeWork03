fc7eccaab10b39986104050b9df3eb6e7ba84702
a1ekP <a1ekc.p85@gmail.com>
2022-07-31 21:42:18 +0300

    add hw

---
+++ app.js
@@ -0,0 +1,47 @@
+let currency = [];
+
+
+document.querySelector('.search').onkeyup = function(event){
+    let value = event.currentTarget.value.toLowerCase();
+    let currencySelect = currency.filter(function(el){
+        const name = el.name.toLowerCase();
+        return name.includes(value);
+    })
+    currencyRender(currencySelect);
+}
+
+
+
+
+
+function currencyRender(currency){
+    let htmlStr = currency.reduce(function(acc, el, index){
+        return acc + `<tr>
+                            <th>${index+1}</th>
+                            <th>${el.name}</th>
+                            <th>${el.rate}</th>
+                            <th>${el.code}</th>
+                            <th>${el.data}</th>
+                    </tr>`
+    }, '');
+    document.querySelector('.table tbody').innerHTML = htmlStr || '<tr><th colspan = "5" class="text-center">Not found</th></tr>';
+}
+
+currencyRender(currency);
+
+
+fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20220731&json').then(function (data){
+    return data.json();
+}).then(function(data) {
+    currency = data.map(function(el) {
+        return {
+            name: el.txt,
+            rate: el.rate,
+            code: el.cc,
+            data: el.exchangedate
+            };
+        });
+        console.log('currency', currency);
+        currencyRender(currency);
+       // renderSelect(countries);
+});
